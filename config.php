2cbd4519ae8e287945233a56aa3ee859bc3193cf
boldry <91291322+boldry@users.noreply.github.com>
2023-09-16 16:44:24 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+# This is an auto-generated config file!
+# Be careful, when manually editing this!
+
+date_default_timezone_set('UTC');
+define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector/");
+define("YSFREFLECTORLOGPREFIX", "YSFReflector");
+define("YSFREFLECTORINIPATH", "/etc/");
+define("YSFREFLECTORINIFILENAME", "YSFReflector.ini");
+define("YSFREFLECTORPATH", "/usr/local/bin/");
+define("TIMEZONE", "America/Chicago");
+define("LOGO", "");
+define("REFRESHAFTER", "15");
+define("SHOWPROGRESSBARS", "on");
+define("SHOWOLDMHEARD", "60");
+define("TEMPERATUREALERT", "on");
+define("TEMPERATUREHIGHLEVEL", "90");
+#define("GDPR", "off");
+?>
